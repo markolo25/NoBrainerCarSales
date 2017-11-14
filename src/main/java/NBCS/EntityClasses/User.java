1be1286d77b43edcbe49a286ff258fdf8d655309
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2017-11-13 22:50:43 -0800

    Hot fix for User entity and refactor bug in request page

--- src/main/java/NBCS/EntityClasses/User.java
+++ src/main/java/NBCS/EntityClasses/User.java
@@ -40,8 +40,10 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String name;
     @NotNull
+    @Column(unique = true)
     private String email;
     @NotNull
     private Integer zipCode;
